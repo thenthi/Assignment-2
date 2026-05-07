ce3f047c727c42b405722afcc6e87d9bdfdb463f
thenthi <s4142732@rmit.edu.vn>
2026-05-08 02:41:57 +1000

    up

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ function updateProgressBar() {
   progressBar.style.width = value + "%";
 }
 // Add other functionalities here
+
+const onOffSound = document.querySelector("#on-off-sound");
+const onOffImg = document.querySelector("#on-off-img");
+audio.removeAttribute("controls");
+//onOffsound.addEventLisrener("click", toggleOnOff);
+function toggleOnOff() {
+  if (audio.muted) {
+    audio.muted = false;
+    audio.volume = 1;
+    volumeSlider.value = 100;
+    onOffImg.src = "https://img.icons8.com/?size=100&id=641&format=png&color=000000";
+  } else {
+    audio.muted = true;
+    volumeSlider.value = 0;
+    onOffImg.src = "https://img.icons8.com/?size=100&id=7900&format=png&color=000000";
+  }
+}
+//volumeslider
+const volumeSlider = document.querySelector("#volume-slider");
+volumeSlider.addEventListener("input", () => {
+  audio.volume = volumeSlider.value / 100;
+
+  if (audio.volume === 0) {
+    audio.muted = true;
+    onOffImg.src = "https://img.icons8.com/?size=100&id=7900&format=png&color=000000";
+  } else {
+    audio.muted = false;
+    onOffImg.src = "https://img.icons8.com/?size=100&id=641&format=png&color=000000";
+  }
+});
